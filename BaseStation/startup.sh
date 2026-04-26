c617483886dd0a17e68191f04a9e3588338ee203
Jared Mayfield <jared@csmayfield.org>
2026-04-26 16:14:12 -0400

    Improve startup script

--- BaseStation/startup.sh
+++ BaseStation/startup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-source wifiConfig.sh # This defines IFACE, SSID, and PASSWRD
+
+# Move to the root folder of the project
+cd "$(dirname "$0")/.."
+source BaseStation/wifiConfig.sh # This defines IFACE, SSID, and PASSWRD
 
 # Clear any hotspot connection and create a new one
 nmcli connection delete Hotspot
 nmcli device wifi hotspot ifname "$IFACE" ssid "$SSID" password "$PASSWRD"
 
 # Start the server
-../pyenv/bin/python station.py
+./pyenv/bin/python BaseStation/station.py
 
 # Stop the hotspot when the script ends
 trap '
 echo "Turning off hotpost..."
 nmcli connection down Hotspot
 nmcli connection delete Hotspot
-' SIGINT SIGTERM EXIT
+' EXIT
